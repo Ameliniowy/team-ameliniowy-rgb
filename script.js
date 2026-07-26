52650eb22ccb5418d699bfa62eb4261dee2b9bca
Ameliniowy <ameliniowy@op.pl>
2026-07-26 05:45:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,43 @@
-/* ==========================================
-   TEAM AMELINIOWY RGB 6.0
-========================================== */
+<!DOCTYPE html>
+<html lang="pl">
 
-const logo = document.getElementById("logo");
-const question = document.getElementById("question");
-const answer = document.getElementById("answer");
+<head>
 
-const colors = [
-    [0,255,80],      // zielony
-    [255,220,0],     // żółty
-    [255,140,0],     // pomarańczowy
-    [255,40,40],     // czerwony
-    [180,50,255],    // fiolet
-    [40,120,255],    // niebieski
-    [0,220,255],     // turkus
-    [0,255,80]
-];
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-const DURATION = 5000;
+<title>Team Ameliniowy RGB 6.3</title>
 
-function lerp(a,b,t){
-    return a + (b-a) * t;
-}
+<link rel="stylesheet" href="style.css">
 
-function getColor(offset){
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-    const total = colors.length - 1;
+<link href="https://fonts.googleapis.com/css2?family=Allura&family=Bebas+Neue:wght@400&display=swap" rel="stylesheet">
 
-    let progress = ((performance.now() / DURATION) + offset) % total;
+</head>
 
-    let current = Math.floor(progress);
-    let next = (current + 1) % colors.length;
+<body>
 
-    let t = progress - current;
+<div id="container">
 
-    const c1 = colors[current];
-    const c2 = colors[next];
+    <div id="logo">
+        Team<br>
+        Ameliniowy
+    </div>
 
-    const r = Math.round(lerp(c1[0], c2[0], t));
-    const g = Math.round(lerp(c1[1], c2[1], t));
-    const b = Math.round(lerp(c1[2], c2[2], t));
+    <div id="question">
+        JAKI KSZTAŁT MA ZIEMIA?
+    </div>
 
-    return `rgb(${r},${g},${b})`;
+    <div id="answer">
+        PŁASKA CZY KULISTA?
+    </div>
 
-}
+</div>
 
-function animate(){
+<script src="script.js"></script>
 
-    /* TEAM */
-    const logoColor = getColor(0);
+</body>
 
-    logo.style.color = logoColor;
-
-    /* PYTANIE */
-    const questionColor = getColor(3.5);
-
-    question.style.color = questionColor;
-
-    /* ODPOWIEDŹ */
-    const answerColor = getColor(4.2);
-
-    answer.style.color = answerColor;
-
-    requestAnimationFrame(animate);
-
-}
-
-animate();
+</html>
